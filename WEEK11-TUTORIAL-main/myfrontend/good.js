57984ef0a68cd0e4f0627fd102c44924096d0d21
PUTTIPONG3035 <88504764+PUTTIPONG3035@users.noreply.github.com>
2026-04-15 22:19:13 +0700

    add good

---
+++ WEEK11-TUTORIAL-main/myfrontend/good.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const good = () => {
+  return (
+    <View>
+      <Text>good</Text>
+    </View>
+  )
+}
+
+export default good
